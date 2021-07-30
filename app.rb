e08fe2a3de71e027ac30f87b3e42db4d413386dd
Gengur123 <meleearthur@gmail.com>
2021-07-30 15:41:57 -0700

    Added edit pages and volunteer pages with full functionality

--- app.rb
+++ app.rb
@@ -21,6 +21,36 @@ get('/projects/new') do
   erb(:new_project)
 end
 
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i)
+  erb(:edit_project)
+end
+
+get('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  @project.delete
+  redirect to('/projects')
+end
+
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i)
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save()
+  erb(:project)
+end
+
 post('/projects') do
   title = params[:project_title]
   project = Project.new({:title => title, :id => nil})
@@ -28,7 +58,22 @@ post('/projects') do
   redirect to('/projects')
 end
 
-get('/projects/:id') do
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  @project.update(params[:project_title])
+  redirect to('/projects')
+end
+
+patch('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i)
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:volunteer_name], @project.id)
+  erb(:project)
+end
+
+delete('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
   @project = Project.find(params[:id].to_i)
   erb(:project)
 end
