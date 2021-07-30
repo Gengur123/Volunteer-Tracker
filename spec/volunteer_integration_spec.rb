69b2e6ae7fa036ce9b1032e6b8f3be4fb8805857
Gengur123 <meleearthur@gmail.com>
2021-07-30 15:57:37 -0700

    All integration test are passing!

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -31,30 +31,31 @@ describe 'the project update path', {:type => :feature} do
   end
 end
 
-# describe 'the project delete path', {:type => :feature} do
-#   it 'allows a user to delete a project' do
-#     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#     test_project.save
-#     id = test_project.id
-#     visit "/projects/#{id}/edit"
-#     click_button('Delete Project')
-#     visit '/'
-#     expect(page).not_to have_content("Teaching Kids to Code")
-#   end
-# end
+describe 'the project delete path', {:type => :feature} do
+  it 'allows a user to delete a project' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    id = test_project.id
+    visit "/projects/#{id}/edit"
+    click_button('Delete project')
+    visit '/'
+    expect(page).not_to have_content("Teaching Kids to Code")
+  end
+end
 
 
-# describe 'the volunteer detail page path', {:type => :feature} do
-#   it 'shows a volunteer detail page' do
-#     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#     test_project.save
-#     project_id = test_project.id.to_i
-#     test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
-#     test_volunteer.save
-#     visit "/projects/#{project_id}"
-#     click_link('Jasmine')
-#     fill_in('name', :with => 'Jane')
-#     click_button('Update Volunteer')
-#     expect(page).to have_content('Jane')
-#   end
-# end
+
+describe 'the volunteer detail page path', {:type => :feature} do
+  it 'shows a volunteer detail page' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    project_id = test_project.id.to_i
+    test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
+    test_volunteer.save
+    visit "/projects/#{project_id}"
+    click_link('Jasmine')
+    fill_in('volunteer_name', :with => 'Jane')
+    click_button('Rename Volunteer')
+    expect(page).to have_content('Jane')
+  end
+end
