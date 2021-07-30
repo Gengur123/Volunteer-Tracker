ad1975f8591651c962a9263a24252db14864922c
Gengur123 <meleearthur@gmail.com>
2021-07-30 08:52:23 -0700

    Added all some new files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
 require 'rspec'
 require 'pg'
+require 'project'
+require 'volunteer'
 require 'pry'
 
-DB = PG.connect({ dbname: 'record_store_test', host: 'db', user: 'postgres', password: 'password' })
+DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    # Add code to clear database.
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
   end
 end
